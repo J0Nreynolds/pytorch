42822077ec7add398332b5f36867fd9b6bba6480
Shen Li <cs.shenli@gmail.com>
2019-11-21 10:53:22 -0800

    Fix BackendType repr in doc
    Before this commit, rpc docs shows init_rpc as the following:
    
    > torch.distributed.rpc.init_rpc(
    >   name,
    >   backend=<BackendType.PROCESS_GROUP: BackendValue(
    >     construct_rpc_agent_options_handler=<function _process_group_construct_rpc_agent_options_handler>,
    >     init_backend_handler=<function _process_group_init_backend_handler>)>,
    >   init_method=None,
    >   rank=-1,
    >   world_size=None,
    >   rpc_agent_options=None
    > )
    
    It unnecessarily leaks implementation details. This commit adds a
    __repr__ function to BackendType Enum class to address this problem.
    
    ghstack-source-id: 71a95e723efe5b1d21d4400078b1cf40ef6c698f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/30243

--- torch/distributed/rpc/backend_registry.py
+++ torch/distributed/rpc/backend_registry.py
@@ -14,8 +14,17 @@ BackendValue = collections.namedtuple(
     "BackendValue", ["construct_rpc_agent_options_handler", "init_backend_handler"]
 )
 
+
+def _backend_type_repr(self):
+    return "BackendType." + self.name
+
+
 # Create an enum type, `BackendType`, with empty members.
-BackendType = enum.Enum(value="BackendType", names={})
+BackendType = enum.Enum(
+    value="BackendType",
+    names={},
+    __repr__=_backend_type_repr
+)
 
 
 def register_backend(
@@ -46,7 +55,11 @@ def register_backend(
         },
         **existing_enum_dict
     )
-    BackendType = enum.Enum(value="BackendType", names=extended_enum_dict)
+    BackendType = enum.Enum(
+        value="BackendType",
+        names=extended_enum_dict,
+        __repr__=_backend_type_repr
+    )
     return BackendType[backend_name]
 
 
